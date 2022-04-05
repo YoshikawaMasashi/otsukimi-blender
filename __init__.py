9ce7af42f5d755de10620394d1027a64ed55d9f9
masashi yoshikawa <earnest.marshi@gmail.com>
2022-04-05 21:16:17 +0900

    empty addon

---
+++ __init__.py
@@ -0,0 +1,27 @@
+import bpy
+import bmesh
+import collections
+from typing import Optional, Any, List
+import math
+
+bl_info = {
+    "name": "otsukimi",
+    "author": "marshi",
+    "description": "otsukimi blender addon",
+    "version": (0, 0, 1),
+    "blender": (2, 90, 0),
+    "location": "",
+    "url": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "marshi"
+}
+
+def register():
+    pass
+
+def unregister():
+    pass
+
+if __name__ == "__main__":
+    register()
